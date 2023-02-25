037561b70e2bd789366f874aba848f759044d6b9
ayinfly <ayinflyschool@gmail.com>
2023-02-25 13:24:47 -0800

    Add game logic in playGame for wins and losses

--- game.js
+++ game.js
@@ -1,7 +1,33 @@
 // Rock Paper Scissors Game
 
 function getComputerChoice() {
-    const compPicks = ["rock", "paper", "scissors"]
-    compPick = compPicks[Math.random()*(3-1)+1]
+    const compPicks = ["Rock", "Paper", "Scissors"]
+    let compPick = compPicks[Math.floor(Math.random()*(3))]
     console.log(compPick)
+}
+
+function playGame(playerChoice, compChoice) {
+    const pChoice = playerChoice.toLowerCase()
+    const cChoice = compChoice.toLowerCase()
+    if (pChoice === cChoice) {
+        console.log(`You Tie! ${playerChoice} ties with ${compChoice}`)
+    } else if (pChoice === "rock") {
+        if (cChoice === "scissors") {
+            console.log(`You Win! ${playerChoice} beats ${compChoice}`)
+        } else {
+            console.log(`You Lose! ${compChoice} beats ${playerChoice}`)
+        }
+    } else if (pChoice === "paper") {
+        if (cChoice === "rock") {
+            console.log(`You Win! ${playerChoice} beats ${compChoice}`)
+        } else {
+            console.log(`You Lose! ${compChoice} beats ${playerChoice}`)
+        }
+    } else if (pChoice === "scissors") {
+        if (cChoice === "paper") {
+            console.log(`You Win! ${playerChoice} beats ${compChoice}`)
+        } else {
+            console.log(`You Lose! ${compChoice} beats ${playerChoice}`)
+        }
+    }
 }
